e4fb489480e99b7ec7e1ef8009bd045437aec589
sunxinqiang <921491709@qq.com>
2017-05-09 20:32:11 +0800

    change the frame to angular

---
+++ js/application/app_route.js
@@ -0,0 +1,37 @@
+/**
+ * Created by sunxinqiang on 2017/5/9.
+ */
+var myApp = angular.module("myApp", ['ui.router']);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state("home", {
+            // abstract: true,
+            url: "/home",
+            templateUrl: "html/home.html",
+            // template:"<div>aaaa</div>"
+            // controller: 'homeCtrl'
+        })
+        .state("background", {
+            url: "/background",
+            templateUrl: "hjx/background.html",
+            controller: 'BackgroundCtrl'
+        })
+        .state("tabs", {
+            url: "/tabs",
+            templateUrl: "module/tabs.html"
+        })
+        .state("tabs.index", {
+            url:"/index",
+            templateUrl: "module/tab.index/index.html",
+            controller:'IndexCtrl'
+        })
+        .state("tabs.view", {
+            url:"/view",
+            templateUrl: "module/tab.view/view.html"
+        })
+        .state("single", {
+            url:"/single",
+            templateUrl: "module/single/single.html"
+        });
+    // $urlRouterProvider.otherwise('/home');
+});
---
+++ js/main.js
@@ -0,0 +1,40 @@
+/**
+ * Created by sunxinqiang on 2017/5/9.
+ */
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        ready: 'public/ready',
+        angular: 'public/angular',
+        ui_router: 'public/angular-ui-router',
+        app_route: 'application/app_route'
+    },
+    shim: {
+        'angular': {
+          exports: 'angular'
+        },
+        'ui_router': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['angular']
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            // exports: 'Backbone'
+        },
+        'app_route': {
+            deps: ['angular']
+        }
+    }
+});
+
+// Start the main app logic.
+require(['ready','angular', 'ui_router', 'app_route'],
+    function(ready,angular) {
+        angular.bootstrap(document, ['myApp']);
+    });
